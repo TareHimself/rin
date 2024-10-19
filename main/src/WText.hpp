6da17f50debda2f5cad4ac655ea3bc80f365c4c2
Tare <75279482+TareHimself@users.noreply.github.com>
2024-10-19 14:11:08 -0400

    Text widget start

--- main/src/WText.hpp
+++ main/src/WText.hpp
@@ -1,8 +1,11 @@
 ﻿#pragma once
+#include "rin/widgets/USDFContainer.hpp"
 #include "rin/widgets/Widget.hpp"
 
 class WText : public Widget
 {
 public:
-    Shared<>
+    USDFContainer sdf{};
+    void CollectContent(const TransformInfo& transform, WidgetDrawCommands& drawCommands) override;
+    Vec2<float> ComputeContentSize() override;
 };
